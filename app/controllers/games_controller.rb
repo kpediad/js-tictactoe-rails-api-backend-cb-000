6f8ca6ec1fb5fe45d996db98bb9caaecf02b5d0b
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-06-27 09:16:52 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,23 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def create
+    @game = Game.create(game_params)
+    render json: @game, status: 201
   end
 
   def index
+    @games = Games.all
+    render json: @games, status: 200
   end
 
   def update
+    @game = Game.update(game_params)
+    render json: @game, status: 201
   end
 
   def show
+    @game = Game.find(params[:id])
+    render json: @game, status: 200
   end
 
 end
